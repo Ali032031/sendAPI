ff17a02628d33028997adfef2f86410cce465e51
adnaneAbdel <adnaneabdellaoui01@gmail.com>
2026-02-15 20:57:10 +0100

    sending

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+}))
 app.post("/send", async (req, res) => {
   try {
     const response = await axios.post(
